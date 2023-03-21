46d77b9244f8987f54aee62194bab8913d1b2c07
Yan Voronkov <sub.voronkov@gmail.com>
2023-03-21 11:17:05 +0300

    Добавлена задача 27

--- Seminars/Task27.py
+++ Seminars/Task27.py
@@ -8,4 +8,10 @@
 # that she sells are sea shells I'm sure.So if she sells sea
 # shells on the sea shore I'm sure that the shells are sea
 # shore shells
-# Output: 13
+# # Output: 13
+
+my_str = "She sells sea shells on the sea shore The shells "\
+    "that she sells are sea shells I'm sure.So if she sells sea "\
+    "shells on the sea shore I'm sure that the shells are sea "\
+    "shore shells"
+print(len(set(my_str.split())))

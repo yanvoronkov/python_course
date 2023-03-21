5c78d1b4a1bcb6130c4e2089ea350fd48718a9ce
Yan Voronkov <sub.voronkov@gmail.com>
2023-03-21 11:40:16 +0300

    Добавлена задача 29

--- Seminars/Task29.py
+++ Seminars/Task29.py
@@ -14,19 +14,27 @@
 # слайдах
 
 # Ваня:
-n = int(input())
-max_number = 1000
-while n != 0:
-    n = int(input())
-    if max_number > n:
-        max_number = n
-print(max_number)
+# n = int(input())
+# max_number = 1000
+# while n != 0:
+#     n = int(input())
+#     if max_number > n:
+#         max_number = n
+# print(max_number)
 
 # Петя:
+# n = int(input())
+# max_number = -1
+# while n < 0:
+#     n = int(input())
+#     if max_number < n:
+#         n = max_number
+# print(n)
+
 n = int(input())
-max_number = -1
-while n < 0:
+max_number = n
+while n != 0:
     n = int(input())
-    if max_number < n:
-        n = max_number
-print(n)
+    if n > max_number:
+        max_number = n
+print(max_number)

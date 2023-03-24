a41102c2e4e43d983e06097117f52a004f8e3d07
Yan Voronkov <sub.voronkov@gmail.com>
2023-03-24 14:41:46 +0300

    Добавлена задача task24

--- Seminars/Task24.py
+++ Seminars/Task24.py
@@ -12,19 +12,36 @@
 # собрать за один заход собирающий модуль, находясь перед некоторым кустом
 # заданной во входном файле грядки.
 
-number_of_bushes = (int(input('Введите количество кустов: ')))
-dict_of_bushes = {}
-for i in range(number_of_bushes):
-    dict_of_bushes[i] = int(input(F"Введите количество ягод на кусте {i+1}: "))
-max_harvest = 0
-for i in dict_of_bushes:
-    if i+2 < len(dict_of_bushes):
-        harvest = dict_of_bushes[i] + dict_of_bushes[i+1] + dict_of_bushes[i+2]
-    if i+2 == len(dict_of_bushes):
-        harvest = dict_of_bushes[i] + dict_of_bushes[i+1] + dict_of_bushes[0]
-    else:
-        harvest = dict_of_bushes[i] + dict_of_bushes[i-3] + dict_of_bushes[i-2]
-    if harvest > max_harvest:
-        max_harvest = harvest
+# number_of_bushes = (int(input('Введите количество кустов: ')))
+# dict_of_bushes = {}
+# for i in 3:
+#     dict_of_bushes[i] = int(input(f"Введите количество ягод на кусте {i+1}: "))
+# max_harvest = 0
+# for i in 3:
+#     if i+2 < len(dict_of_bushes):
+#         harvest = dict_of_bushes[i] + dict_of_bushes[i+1] + dict_of_bushes[i+2]
+#     if i+2 == len(dict_of_bushes):
+#         harvest = dict_of_bushes[i] + dict_of_bushes[i+1] + dict_of_bushes[0]
+#     else:
+#         harvest = dict_of_bushes[i] + dict_of_bushes[i-3] + dict_of_bushes[i-2]
+#     if harvest > max_harvest:
+#         max_harvest = harvest
 
-print(max_harvest)
+# print(max_harvest)
+
+n = int(input('Введите число кустов: '))
+a = list(map(int, input(f"Введите чисел {n} в строку через пробел: ").split()))
+
+max_sum = 0
+for i in range(n):
+    print(i)
+    if i == 0:
+        curr_sum = a[i] + a[i + 1] + a[n - 1]
+    elif i == n - 1:
+        curr_sum = a[i] + a[i - 1] + a[0]
+    else:
+        curr_sum = a[i] + a[i - 1] + a[i + 1]
+    if curr_sum > max_sum:
+        max_sum = curr_sum
+
+print(max_sum)
